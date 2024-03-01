d8a3aba2fca6d8dbc98cc05cbceac6e97cc706fd
Eduard <eddnovik@gmail.com>
2024-03-01 14:49:53 +0100

    remove all

--- theme/assets/designMode.js
+++
@@ -1,7 +0,0 @@
-if(typeof BoosterTheme != "undefined"){
-    document.addEventListener('shopify:section:load', ()=>BoosterTheme.events.trigger('booster:content:update'))
-} else {
-    document.addEventListener('booster:loaded', ()=>{
-        document.addEventListener('shopify:section:load', ()=>BoosterTheme.events.trigger('booster:content:update'))
-    })
-}
