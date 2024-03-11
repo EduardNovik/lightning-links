199f3ddb53c35556eb3a14d0ce181df7b598071e
Eduard <eddnovik@gmail.com>
2024-03-11 21:37:44 +0100

    fix

--- assets/customScript.js
+++ assets/customScript.js
@@ -4,14 +4,14 @@ const forPartnersPopup = document.getElementById("for-partners-popup");
 const closePopupButton = document.getElementById("closePopupButton");
 const cancelPopupButton = document.getElementById("cancelButton");
 
-const buyPopupCustomButtons = document.querySelectorAll(
-  ".buyPopupCustomButtons"
-);
-const buyPopupCustom = document.getElementById("buy-popup-custom");
+// const buyPopupCustomButtons = document.querySelectorAll(
+//   ".buyPopupCustomButtons"
+// );
+// const buyPopupCustom = document.getElementById("buy-popup-custom");
 
-const closeBuyPopupCustomButton = document.getElementById(
-  "closeBuyPopupCustomButton"
-);
+// const closeBuyPopupCustomButton = document.getElementById(
+//   "closeBuyPopupCustomButton"
+// );
 
 const successPopup = document.getElementById("successPopup");
 const sendButtonInForPartnersPopup = document.getElementById(
@@ -66,16 +66,40 @@ if (quickOrderBtn) {
   });
 }
 
-buyPopupCustomButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    buyPopupCustom.classList.toggle("hidden");
-    buyPopupCustom.classList.toggle("flex");
-    body.classList.toggle("overflow-hidden");
+// buyPopupCustomButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     buyPopupCustom.classList.toggle("hidden");
+//     buyPopupCustom.classList.toggle("flex");
+//     body.classList.toggle("overflow-hidden");
+//   });
+// });
+
+// closeBuyPopupCustomButton.addEventListener("click", () => {
+//   buyPopupCustom.classList.toggle("hidden");
+//   buyPopupCustom.classList.toggle("flex");
+//   body.classList.toggle("overflow-hidden");
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buyButtons = document.querySelectorAll(".buyPopupCustomButton");
+
+  buyButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const popupId = this.getAttribute("data-popup-id");
+      const popup = document.getElementById(popupId);
+      if (popup) {
+        popup.classList.remove("hidden");
+        // Additional logic to properly show the popup, e.g., adding overlay, disabling background scroll, etc.
+      }
+    });
   });
 });
 
-closeBuyPopupCustomButton.addEventListener("click", () => {
-  buyPopupCustom.classList.toggle("hidden");
-  buyPopupCustom.classList.toggle("flex");
-  body.classList.toggle("overflow-hidden");
+document.addEventListener("DOMContentLoaded", function () {
+  // Close popup functionality
+  document.querySelectorAll("#closeBuyPopupCustomButton").forEach((button) => {
+    button.addEventListener("click", function () {
+      this.closest(".buy-popup-custom").classList.add("hidden");
+    });
+  });
 });
